4f8fc21a51bc776747454f054887d9d390ecd5bb
Alaina Mupparthi <asm1075@rit.edu>
2024-03-21 12:07:28 -0400

    Added menu

---
+++ src/menu.java
@@ -0,0 +1,24 @@
+package src;
+
+public class menu {
+    public static void main(String[] args) {
+        System.out.println("1) create an account\n" +
+                "     * 2) log in\n" +
+                "     * 3) view collections\n" +
+                "     *      a) edit collection\n" +
+                "     *          - edit collection name\n" +
+                "     *          - delete game\n" +
+                "     *      b) create collection\n" +
+                "     *      c) delete collection\n" +
+                "     * 4) search games\n" +
+                "     *      a) sort by\n" +
+                "     *      b) add game to collection\n" +
+                "     *      c) play game - make sure player owns platform\n" +
+                "     *      d) rate game\n" +
+                "     * 5) Play a random game\n" +
+                "     * 6) Search friends\n" +
+                "     *      a) follow\n" +
+                "     *      b) unfollow");
+    }
+
+}
